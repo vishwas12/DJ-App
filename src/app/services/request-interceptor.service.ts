68d51c81437e59846f338ec62a7dc2486333219b
Vishwas Ganguly <vishwasg@dewsolutions.in>
2017-09-27 00:14:18 +0530

    request interceptor/ auth gaurd/ vendor details ui

--- src/app/services/request-interceptor.service.ts
+++ src/app/services/request-interceptor.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest } from '@angular/common/http';
+import { HttpInterceptor, HttpEvent, HttpHandler, HttpRequest, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs/Rx';
 
 @Injectable()
@@ -9,7 +9,17 @@ export class RequestInterceptorService implements HttpInterceptor {
     /* if (req.url.includes('api.github.com')) {
       const clone = req.clone({ setHeaders: { 'Authorization': `Bearer ${OAUTH_TOKEN}` } });
     } */
-    return next.handle(req);
+    // return next.handle(req);
+    return next.handle(req)
+    .do(event => {
+      if (event instanceof HttpResponse) {
+        console.log(event);
+      }
+    })
+    .catch(err => {
+      console.log('Caught error', err);
+      return Observable.throw(err);
+    });
   }
 
 }
